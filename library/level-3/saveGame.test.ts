cf594c203da99d742cf2ba7abfefadfd87204bac
wvbe <wybe@x-54.com>
2024-02-21 09:02:42 +0100

    OK this is cool I guess

--- library/level-3/saveGame.test.ts
+++ library/level-3/saveGame.test.ts
@@ -7,10 +7,18 @@ import { DEFAULT_ASSETS } from '@lib';
 describe('Save game', () => {
 	const { game } = createGeneratorDemo(new TestDriver());
 
-	it('Loading the initial game', () => {
+	describe('Loading the initial game', async () => {
 		const json = game.toSaveJson();
 		const game2 = Game.fromSaveJson(DEFAULT_ASSETS, json);
-		expect(game2).toEqual(game);
+		// expect(game2).toEqual(game);
+		describe('Equal entities', () => {
+			it('Has the same amount of entities', () => {
+				expect(game2.entities.length).toBe(game.entities.length);
+			});
+			game.entities.forEach((entity, i) =>
+				it(`Entity "${entity.label}"`, () => expect(game2.entities.get(i)).toEqual(entity)),
+			);
+		});
 	});
 });
 

016258045e956bb86c8315be62eb21f9b813d5fa
wvbe <git@wy.be>
2025-04-19 11:47:47 +0200

    before

--- src/ui2/hud/RoundGlass.tsx
+++ src/ui2/hud/RoundGlass.tsx
@@ -6,7 +6,7 @@ export const RoundGlass: FunctionComponent<PropsWithChildren<{ background?: stri
 	background = 'linear-gradient(135deg, #f5f5f5 0%, #e0e0e0 100%)',
 }) => {
 	return (
-		<div className='round-glass-container'>
+		<div className='round-glass-container' style={{ '--zoom-factor': 0.3 }}>
 			<div className='round-glass-face'></div>
 			<div className='round-glass-reflection'></div>
 			<div className='round-glass-content' style={{ background }}>
